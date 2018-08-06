b5e3dbac414ed6b7a22b46e9021f5cb6e9f4a3be
Hooman <h1999190@gmail.com>
2018-08-06 08:35:06 +0430

    Add project files.

---
+++ FileManagerApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FileManagerApp.Startup))]
+namespace FileManagerApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
